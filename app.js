486efb844481926853b02d70bb4661b2e17c579e
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-07-03 11:14:32 -0400

    Changing port number and .env file variables

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/dataBaseKey');
 const bodyParser = require('body-parser');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
-const port  = process.env.PORT || 5000
+const port  = process.env.PORT || 3000
 
 
 //Connect to Database
