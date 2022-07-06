15c40489d203b982548f997ce0c0c123b62f8def
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-07-06 16:42:00 -0400

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,7 +78,6 @@ const getMe = asyncHandler(async(req, res) => {
             var decoded = jwt_decode(token)
             var userValue = Object.values(decoded)
             var user = await User.findOne({username: decoded.username, full_name: decoded.full_name})
-            console.log(`USER: ${user.username} -- FULL NAME: ${user.full_name}`)
             console.log(`DECODED USER: ${decoded.username} -- DECODEDFULL NAME: ${decoded.full_name}`)
             var encoded = jwt_encode({username: user.username, full_name: user.full_name}, process.env.ELEOS_KEY, 'HS256')
             //Just for terminal use
