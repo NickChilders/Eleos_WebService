9f0a5d4b1a9307ae06f4aa799f8f1c5eb1fb21aa
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-07-08 15:43:36 -0400

    Update to load model

--- models/loadModel.js
+++ models/loadModel.js
@@ -1,41 +1,49 @@
 const mongoose = require('mongoose')
 
-const loadSchema = mongoose.Schema({
-    id: {
-        type: String,
-        required: true
+const loadSchema = new mongoose.Schema(
+    {
+        id: {
+            type: String,
+            required: true
+        },
+        display_identifier: {
+            type: String,
+            required: true
+        },
+        sort: {
+            type: Number,
+            required: true
+        },
+        order_number: {
+            type: String,
+            required: true
+        },
+        load_status: {
+            type: String,
+            required: true
+        },
+        load_status_label: {
+            type: String,
+            required: true
+        },
+        active: {
+            type: Boolean,
+            required: true
+        },
+        current: {
+            type: Boolean,
+            required: true
+        }
     },
-    display_identifier: {
-        type: String,
-        required: true
-    },
-    sort: {
-        type: Number,
-        required: true
-    },
-    order_number: {
-        type: String,
-        required: true
-    },
-    load_status: {
-        type: String,
-        required: true
-    },
-    load_status_label: {
-        type: String,
-        required: true
-    },
-    active: {
-        type: Boolean,
-        required: true
-    },
-    current: {
-        type: Boolean,
-        required: true
+    {
+        toJSON: {
+            transform(doc, ret){
+                delete ret._id;
+                return ret;
+            },
+            versionKey: false,
+        },
     }
-},
-{
-    timestamps: true
-})
+);
 
 module.exports = mongoose.model('Loads', loadSchema)
